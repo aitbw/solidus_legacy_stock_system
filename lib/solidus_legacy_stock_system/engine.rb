b883c230db66739e4d8f76b5947efd567ee695a7
swcraig <sebastian@stembolt.com>
2017-09-13 14:43:44 -0700

    Configure core to use the legacy stock coordinator

--- lib/solidus_legacy_stock_system/engine.rb
+++ lib/solidus_legacy_stock_system/engine.rb
@@ -4,6 +4,10 @@ module SolidusLegacyStockSystem
     isolate_namespace Spree
     engine_name 'solidus_legacy_stock_system'
 
+    initializer 'spree.stock.coordinator_class' do |app|
+      app.config.spree.preferences.stock.coordinator_class = 'Spree::Stock::Coordinator'
+    end
+
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
