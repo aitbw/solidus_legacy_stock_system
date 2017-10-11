76aba517a9cb77c0cbe0be6006c610e8aa234db3
swcraig <sebastian@stembolt.com>
2017-10-11 09:54:05 -0700

    Use explicit naming in the `Splitter::Base` spec
    Sometimes the spec thought that we were referring to the `Spree::Base`
    class which was causing the spec to fail.

--- spec/models/spree/stock/splitter/base_spec.rb
+++ spec/models/spree/stock/splitter/base_spec.rb
@@ -8,8 +8,8 @@ module Spree
         let(:stock_location) { mock_model(Spree::StockLocation) }
 
         it 'continues to splitter chain' do
-          splitter1 = Base.new(stock_location)
-          splitter2 = Base.new(stock_location, splitter1)
+          splitter1 = Spree::Stock::Splitter::Base.new(stock_location)
+          splitter2 = Spree::Stock::Splitter::Base.new(stock_location, splitter1)
           packages = []
 
           expect(splitter1).to receive(:split).with(packages)
@@ -17,7 +17,9 @@ module Spree
         end
 
         it 'accepts a packer (deprecated)' do
-          splitter = Spree::Deprecation.silence { Base.new(packer) }
+          splitter = Spree::Deprecation.silence do
+            Spree::Stock::Splitter::Base.new(packer)
+          end
 
           expect(splitter.stock_location).to eq(packer.stock_location)
         end
